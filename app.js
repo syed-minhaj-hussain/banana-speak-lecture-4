20f6a65df7a24982378a04ffe68edb512a8b1d9f
syed-minhaj-hussain <syed-minhaj-hussain@users.noreply.github.com>
2020-12-09 02:38:17 +0530

    ex-4 and live ex

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-// exercise-3
+/*  // exercise-3
 var btn = document.querySelector('#btn');
 console.log(btn);
 
 // define what operation to be taken placce
-    function onClick() {
-        console.log("you Just clicked");
-    }
+function onClick() {
+    console.log("you Just clicked");
+}
 // btn.addEventListener('click', onClick);
+// In ES-6
 btn.addEventListener('click', () => {
     console.log('You Just Clicked');
-}) 
+}) */
+
+// exercise-4
+var btn = document.querySelector('#btn');
+var textArea = document.querySelector('#text-area');
+console.log(textArea);
+// read value from textarea 
+function readValue(){
+    var readUserValue = textArea.value;
+    console.log(readUserValue);
+}
+btn.addEventListener('click', readValue)
+
 
