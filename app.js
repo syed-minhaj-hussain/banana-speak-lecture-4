d0bb72558de43b03de7b0af25a699a0b62ebc5a8
syed-minhaj-hussain <syed-minhaj-hussain@users.noreply.github.com>
2020-12-09 12:54:30 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,26 +1,2 @@
-// Ex-9
-   var btn = document.querySelector('#btn');
-   var textArea = document.querySelector('#text-area');
-   var div = document.querySelector('.box');
-//    var takeTextAreaValue = textArea.value;
-
-   var url = "https:api.funtranslations.com/translate/minion.json";
-
-    function getTranslationUrl(text) {
-      return  url + "?" + "text=" + text;
-    }
-
-   function clickEventHandler() {
-    //    var inputFromTextArea = textArea.value;
-       fetch(getTranslationUrl(textArea.value))
-       .then( response => response.json())
-       .then( json => console.log(json) )
-       .catch( error => {
-        alert(error + " try after some time")
-    })
-    div.innerText = textArea.value;
-   }
-
-   btn.addEventListener('click', clickEventHandler);
-
-
+// grab the necessary documents
+var textArea;
