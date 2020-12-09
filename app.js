fcc2142bea0bf01de60c8cd932dc8c5d01ede585
syed-minhaj-hussain <syed-minhaj-hussain@users.noreply.github.com>
2020-12-09 13:04:11 +0530

    update .js

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 // grab the necessary documents
-var textArea;
+var textArea = document.querySelector('#text-area');
+var btn = document.querySelector('#btn');
+var box = document.querySelector('.box');
+
+// grab the url
+var minionUrl = "https://api.funtranslations.com/translate/minion.json";
+
+// function for concatination
+function concatinateUrl() {
+    return minionUrl + "?" + "text=" + text;
+}
+
+// get value from textarea
+function getValue() {
+    var input = textArea.value;
+    fetch(concatinateUrl(input))
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+
+btn.addEventListener('click', getValue)
